bc48432541e7ba10c55cb8f73eaeb9304266399b
Alexey <alexey.vovk@simplinic.com>
2023-08-15 12:52:26 +0300

    add: basic classes

---
+++ src/main/java/edu/praktikum/Courier.java
@@ -0,0 +1,5 @@
+package edu.praktikum;
+
+public class Courier {
+
+}
---
+++ src/test/java/edu/praktikum/CourierTests.java
@@ -0,0 +1,16 @@
+package edu.praktikum;
+
+import org.junit.Test;
+
+// https://practicum.yandex.ru/webinar-form/qa-automation-engineer-java
+
+// https://qa-scooter.praktikum-services.ru
+// api/v1/courier
+// api/v1/courier/login
+public class CourierTests {
+
+    @Test
+    public void createCourier() {
+
+    }
+}
